1b88639517665e9b0aeaef009351c5768d6acda2
h66m9d <h66m9d@users.noreply.github.com>
2018-02-08 15:10:17 +0330

    Update ydl-channel-list.sh

--- ydl-channel-list.sh
+++ ydl-channel-list.sh
@@ -5,14 +5,14 @@ cd $randomdir
 read -p "Enter Channel Playlist URL: " channelplaylisturl
 curl $channelplaylisturl > /tmp/page
 sed -n 's/.*href="\([^"]*\).*/\1/p' /tmp/page | grep "?list" | cut -d"=" -f2 | sort -u > /tmp/list
-youtube-dl --yes-playlist -r 4096k -o "%(playlist_uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s" --download-archive "../downloaded.txt" --ignore-errors $listurl --batch-file /tmp/list
+youtube-dl --yes-playlist -r 4096k -o "%(playlist_uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s" --download-archive "../$randomdirdownloaded.txt" --ignore-errors $listurl --batch-file /tmp/list
 youtube-dl --yes-playlist -r 4096k -o "%(playlist_uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s" --ignore-errors $listurl --batch-file /tmp/list
 youtube-dl --yes-playlist -r 4096k -o "%(playlist_uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s" --ignore-errors $listurl --batch-file /tmp/list
-youtube-dl --yes-playlist -r 4096k -o "%(playlist_uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s" --download-archive "../downloaded.txt" --ignore-errors $listurl --batch-file /tmp/list
+youtube-dl --yes-playlist -r 4096k -o "%(playlist_uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s" --download-archive "../$randomdirdownloaded.txt" --ignore-errors $listurl --batch-file /tmp/list
 ls > /tmp/ydlls
 playlistuploadername=`cat /tmp/ydlls`
 playlistuploadernamenonespace=`echo $playlistuploadername | tr ' ' '_'`
-tar -czvf ../$playlistuploadernamenonespace.tar.gz * ../downloaded.txt
+tar -czvf ../$playlistuploadernamenonespace.tar.gz * ../$randomdirdownloaded.txt
 cd ..
-rm downloaded.txt
+rm $randomdirdownloaded.txt
 rm -r $randomdir
