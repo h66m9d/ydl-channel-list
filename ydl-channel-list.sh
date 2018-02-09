12f3de700d5ee7523104487685ed0992c9fc9f94
h66m9d <h66m9d@users.noreply.github.com>
2018-02-10 00:50:11 +0330

    Update ydl-channel-list.sh

--- ydl-channel-list.sh
+++ ydl-channel-list.sh
@@ -9,7 +9,7 @@ read -p "Playlist(1) OR Channel(2) or SingleVideo(3)? [1/2/3]: " -e -i 2 todo
 	if [[ "$todo" = '1' ]]; then
 		addtoplaylisttrue="y"
 		while [  $addtoplaylisttrue == "y" ]; do
-			read -p "Enter Playlist URL: " playlisturl
+			read -p "Enter Playlist URL (eg: https://www.youtube.com/watch?v=nfWlot6h_JM&list=PLmU8B4gZ41idW0H82OGG8nvlkceNPqpvq): " playlisturl
 			echo $playlisturl | cut -d"=" -f3 >> /tmp/list
 			read -p "Add more? [y/n] " -e -i "y" addtoplaylisttrue
 		done
@@ -17,7 +17,7 @@ read -p "Playlist(1) OR Channel(2) or SingleVideo(3)? [1/2/3]: " -e -i 2 todo
 		videofilename="%(playlist_uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s"
 		yesplaylist="--yes-playlist"
 	elif [[ "$todo" = '2' ]]; then
-		read -p "Enter Channel Playlist URL: " channelplaylisturl
+		read -p "Enter Channel Playlists URL (eg: https://www.youtube.com/user/VEVO/playlists): " channelplaylisturl
 		curl $channelplaylisturl > /tmp/page
 		sed -n 's/.*href="\([^"]*\).*/\1/p' /tmp/page | grep "?list" | cut -d"=" -f2 | sort -u > /tmp/list
 		cat /tmp/list > grep "PL" | sort -u > /tmp/list
@@ -26,7 +26,7 @@ read -p "Playlist(1) OR Channel(2) or SingleVideo(3)? [1/2/3]: " -e -i 2 todo
 	elif [[ "$todo" = '3' ]]; then
 		addtoplaylisttrue="y"
 		while [  $addtoplaylisttrue == "y" ]; do
-			read -p "Enter video URL: " videourl
+			read -p "Enter video URL (eg: https://www.youtube.com/watch?v=nfWlot6h_JM): " videourl
 			echo $videourl | cut -d"=" -f2 | cut -d"&" -f1 >> /tmp/list
 			videofilename="%(uploader)s/%(title)s.%(ext)s"
 			read -p "Add more? [y/n] " -e -i "y" addtoplaylisttrue
