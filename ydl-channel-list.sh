32dbfba4e76940b80c9297cc921c33c9f22ebcb8
h66m9d <h66m9d@users.noreply.github.com>
2018-02-10 00:27:25 +0330

    Update ydl-channel-list.sh

--- ydl-channel-list.sh
+++ ydl-channel-list.sh
@@ -6,31 +6,37 @@ archivenamenonespace=`echo $archivename | tr ' ' '_'`
 mkdir $archivenamenonespace
 cd $archivenamenonespace
 read -p "Playlist(1) OR Channel(2) or SingleVideo(3)? [1/2/3]: " -e -i 2 todo
-      if [[ "$todo" = '1' ]]; then
+	if [[ "$todo" = '1' ]]; then
 		addtoplaylisttrue="y"
 		while [  $addtoplaylisttrue == "y" ]; do
 			read -p "Enter Playlist URL: " playlisturl
-            		echo $playlisturl | cut -d"=" -f3 >> /tmp/list
-            		read -p "Add more? [y/n] " -e -i "y" addtoplaylisttrue
+			echo $playlisturl | cut -d"=" -f3 >> /tmp/list
+			read -p "Add more? [y/n] " -e -i "y" addtoplaylisttrue
 		done
-            videofilename="%(playlist_uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s"
-            yesplaylist="--yes-playlist"
-      fi
-      if [[ "$todo" = '2' ]]; then
-            read -p "Enter Channel Playlist URL: " channelplaylisturl
-            curl $channelplaylisturl > /tmp/page
-            sed -n 's/.*href="\([^"]*\).*/\1/p' /tmp/page | grep "?list" | cut -d"=" -f2 | sort -u > /tmp/list
-            videofilename="%(playlist_uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s"
-            yesplaylist="--yes-playlist"
-      fi
-      if [[ "$todo" = '3' ]]; then
-            read -p "Enter video URL: " videourl
-            echo $videourl | cut -d"=" -f2 >> /tmp/list
-            videofilename="%(uploader)s/%(title)s.%(ext)s"
-            yesplaylist=""
-      fi
+		cat /tmp/list > grep "PL" | sort -u > /tmp/list
+		videofilename="%(playlist_uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s"
+		yesplaylist="--yes-playlist"
+	fi
+	if [[ "$todo" = '2' ]]; then
+		read -p "Enter Channel Playlist URL: " channelplaylisturl
+		curl $channelplaylisturl > /tmp/page
+		sed -n 's/.*href="\([^"]*\).*/\1/p' /tmp/page | grep "?list" | cut -d"=" -f2 | sort -u > /tmp/list
+		cat /tmp/list > grep "PL" | sort -u > /tmp/list
+		videofilename="%(playlist_uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s"
+		yesplaylist="--yes-playlist"
+	fi
+	if [[ "$todo" = '3' ]]; then
+		addtoplaylisttrue="y"
+		while [  $addtoplaylisttrue == "y" ]; do
+			read -p "Enter video URL: " videourl
+			echo $videourl | cut -d"=" -f2 | cut -d"&" -f1 >> /tmp/list
+			videofilename="%(uploader)s/%(title)s.%(ext)s"
+			read -p "Add more? [y/n] " -e -i "y" addtoplaylisttrue
+		done
+		yesplaylist=""
+	fi
 for x in seq 1 2 ; do
-youtube-dl $yesplaylist -r 8192k -o $videofilename --download-archive "$archivenamenonespace.downloaded.txt" --ignore-errors --batch-file /tmp/list
+	youtube-dl $yesplaylist -r 8192k -o $videofilename --download-archive "$archivenamenonespace.downloaded.txt" --ignore-errors --batch-file /tmp/list
 done
 yesplaylist=""
 cd ..
